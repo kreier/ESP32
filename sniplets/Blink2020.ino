5e3f4832e95929e9e97454e586e941eac8f23f5f
Matthias Kreier <43933271+kreier@users.noreply.github.com>
2020-07-10 22:12:43 +0700

    updated list of boards

--- sniplets/Blink2020.ino
+++ sniplets/Blink2020.ino
@@ -1,18 +1,18 @@
 /* Blink */
-// T-Koala:    5
-// Wemos lite: 4
-// LORA915:    2   and NodeMCU, D1 R32 (Arduino size)
-// T8 V1.7:    21
-// T-Dislay:   
-// ATMega2560: 13  and Arduino Uno, Leonardo
+// LORA915:        2   and ESP8266 NodeMCU, D1 R32 (Arduino size), DOIT ESP32 DEVKIT V1
+// Wemos LOLIN32:  4
+// T-Koala:        5   and T-Energy, 
+// ATMega2560:    13   and Arduino Uno, Leonardo
+// T8 V1.7:       21
+// WeMos lite:    22
+// TTGO LoRa      25  (not populated)
 
 
 // int ledPin = LED_BUILTIN;
-int ledPin = 13;
-bool light = HIGH; // LORA915, T-Koala, T8, Arduino
-bool dark  = LOW;
-//bool light = LOW; // WEMOS Lite, ESP8266 NODEMCU
-//bool dark  = HIGH;
+int ledPin = 5;
+bool light = HIGH;    // LORA915, T-Koala, T8, Arduino
+//bool light = LOW;   // WEMOS Lite, ESP8266 NODEMCU
+bool dark  = !light;
 int count = 0; 
 
 void setup() {
