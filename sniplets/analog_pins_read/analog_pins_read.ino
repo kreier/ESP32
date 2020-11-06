4148255a9c78e148f345a4455cf5fa5a677d6990
Matthias Kreier <43933271+kreier@users.noreply.github.com>
2020-11-07 00:00:00 +0700

    Update analog_pins_read.ino

--- sniplets/analog_pins_read/analog_pins_read.ino
+++ sniplets/analog_pins_read/analog_pins_read.ino
@@ -15,20 +15,46 @@ void setup() {
 }  
 
 void loop() {
-  for(int i = 0; i < 8; i++) {
-    raw = 0;
-    for(int multisample = 0; multisample < 100; multisample++) {
-      raw += analogRead( adc1[i] );
-    }
-    voltage = 0.826 * raw + 150;
-    Serial.print("ADC1_CH");
-    Serial.print(i);
-    Serial.print(" raw: ");
-    Serial.print(raw / 1000);
-    Serial.print(" voltage: ");
-    Serial.println(voltage);
-  }
+  measureADC( 1 );
+  measureADC( 2 );
+  Serial.println("---- finished ------");
   digitalWrite(ledPin, 0);
-  delay(1000);
+  delay(10000);
   digitalWrite(ledPin, 1);
+}
+
+void measureADC(int adc) {
+  int limitChannels = 8;
+  int pinMeasure;
+  char* string1 = "ADC1_CH";
+  if(adc == 2) {
+    limitChannels = 10;
+    string1 = "ADC2_CH";
+  }
+  for(int i = 0; i < limitChannels; i++) {
+    raw = 0;
+    if(adc == 1) {
+      pinMeasure = adc1[i];
+    } else {
+      pinMeasure = adc2[i];
+    }
+    for(int multisample = 0; multisample < 100; multisample++) {
+      raw += analogRead( pinMeasure );
+    }
+    voltage = 0.00826 * raw + 150;
+    Serial.print(string1);
+    printSerial(i);
+    Serial.print(" raw: ");
+    Serial.print(raw / 100);
+    Serial.print(" voltage: ");
+    printSerial(voltage);
+    Serial.println(" mV");
+  }  
+}
+
+void printSerial(int value) {
+  if(value < 10) Serial.print(" ");
+  if(value < 100) Serial.print(" ");
+  if(value < 1000) Serial.print(" ");
+  Serial.print(value);   
 }
